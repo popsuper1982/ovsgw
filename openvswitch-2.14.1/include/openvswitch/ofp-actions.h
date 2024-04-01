41c201d2160df47adb06eefbf1799ed93648a22c
popsuper1982 <popsuper1982@qq.com>
2024-04-01 16:24:15 +0800

    OFPACT(CONFIG_GW,       ofpact_config_gw,   ofpact, config_gw) forget \

--- openvswitch-2.14.1/include/openvswitch/ofp-actions.h
+++ openvswitch-2.14.1/include/openvswitch/ofp-actions.h
@@ -95,7 +95,7 @@ struct vl_mff_map;
     OFPACT(POP_MPLS,        ofpact_pop_mpls,    ofpact, "pop_mpls")     \
     OFPACT(DEC_NSH_TTL,     ofpact_null,        ofpact, "dec_nsh_ttl")  \
     OFPACT(DELETE_FIELD,    ofpact_delete_field, ofpact, "delete_field") \
-    //XXXXXX 2.define openvswitch action
+    //XXXXXX 2.define openvswitch action                                \
     OFPACT(CONFIG_GW,       ofpact_config_gw,   ofpact, "config_gw")  \
     OFPACT(HANDLE_GW,       ofpact_handle_gw,   ofpact, "handle_gw") \
                                                                         \
